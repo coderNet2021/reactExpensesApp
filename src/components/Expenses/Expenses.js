f6ffd6d43f1f19da8edf45bc1e29a521186d2d66
Lilyane Fawaz <cnss.saida@gmail.com>
2021-07-13 01:29:57 +0300

    filtered by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,14 +11,19 @@ const Expenses = (props) => {
     console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onSelectingChange={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
